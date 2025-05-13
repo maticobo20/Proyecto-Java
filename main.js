5f7d62cfc736817b23374a38730086dc359f1649
Matias Cobo <maticobo20@gmail.com>
2025-05-13 18:10:49 -0300

    practicando variables

---
+++ main.js
@@ -0,0 +1,15 @@
+//Number
+let edad = 26;
+
+// String
+let nombre = "Matías";
+
+// Boolean
+let esEstudiante = true;
+
+
+
+// Consola
+console.log ("Edad:", edad);
+console.log ("Nombre:", nombre);
+console.log ("¿Es estudiante?", esEstudiante);
